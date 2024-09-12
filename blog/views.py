cd571ab93ceceec03a097da25dca85603b6b3e0b
FaysalXulhash <faysalahammed80@gmail.com>
2024-09-12 12:56:29 +0600

    added blog post pagination and user post pagination

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
+from django.contrib.auth.models import User
 from django.contrib.auth.mixins import (
     LoginRequiredMixin, 
     UserPassesTestMixin
 )
-# Create your views here.
 from django.views.generic import (
     ListView, 
     DetailView,
@@ -13,16 +13,31 @@ from django.views.generic import (
     DeleteView,
 )
 
+#List View
 class PostListView(ListView):
     model = Post
     template_name = "blog/home.html"
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 3
+
+#User List View
+class UserPostListView(ListView):
+    model = Post
+    template_name = "blog/user_posts.html"
+    context_object_name = 'posts'
+    paginate_by = 3
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username = self.kwargs.get('username'))
+        return Post.objects.filter(author = user).order_by('-date_posted')
     
 
+#detail view
 class PostDetailView(LoginRequiredMixin, DetailView):
     model = Post
 
+#Create View
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
@@ -30,6 +45,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
+#Update View
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
@@ -42,7 +58,8 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         if self.request.user == post.author:
             return True 
         return False
-    
+
+#Delete View  
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     success_url = '/'
@@ -51,8 +68,6 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         if self.request.user == post.author:
             return True 
         return False
-        
-
 
 '''def home(request):
     context = {
